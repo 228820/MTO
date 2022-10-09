b5c2d5f1bfbfb190ec8fbc2edddab267248d2aeb
Piotr Orczykowski <228820@edu.p.lodz.pl>
2022-10-09 14:25:33 +0200

    Fix code

--- lab2/main.c
+++ lab2/main.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 #include <string.h>
 
-int my_printf(char *format_string, char *param){
-	for(int i=0;i<strlen(format_string);i++){
-		if((format_string[i] == '#') && (format_string[i+1] == 'k')){
-			i++;
-			printf("%s",param);
-		} else {
-			char letter = format_string[i];
-			if(letter > 65 && letter < 90) {
-				letter = letter + 32;
-			} else if(letter > 97 && letter < 122){
-				letter = letter - 32;
-			}
-			putchar(letter);
-			
-		}
-			
-	}
-	puts("");
+int my_printf(char *format_string, char *param) {
+  for (int i = 0; i < strlen(format_string); i++) {
+    if ((format_string[i] == '#') && (format_string[i + 1] == 'k')) {
+      i++;
+      for (int j = 0; j < strlen(param); j++) {
+        char letter = param[j];
+        if (letter > 65 && letter < 90) {
+          letter = letter + 32;
+        } else if (letter > 97 && letter < 122) {
+          letter = letter - 32;
+        }
+        putchar(letter);
+      }
+    } else {
+      putchar(format_string[i]);
+    }
+  }
+  puts("");
 }
 
-int main(int argc, char *argv[]){
-	char buf[1024],buf2[1024];
-	while(gets(buf)){
-		gets(buf2);
-		my_printf(buf,buf2);
-	}
-	return 0;
+int main(int argc, char *argv[]) {
+  char buf[1024], buf2[1024];
+  while (gets(buf)) {
+    gets(buf2);
+    my_printf(buf, buf2);
+  }
+  return 0;
 }
