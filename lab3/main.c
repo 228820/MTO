cb614b51026afe792656ff683f5ddb6de2d12daa
Piotr Orczykowski <228820@edu.p.lodz.pl>
2022-10-23 15:34:15 +0200

    First code refactor

--- lab3/main.c
+++ lab3/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 void printChangedLetter(char letter) {
   if (letter > 65 && letter < 90) {
@@ -11,95 +11,94 @@ void printChangedLetter(char letter) {
   putchar(letter);
 }
 
+void clearArray(char *arr) {
+  for (int i = 0; i < strlen(arr); i++) {
+    arr[i] = '\0';
+  }
+}
+
 int my_printf(char *format_string, char *param) {
-
   for (int i = 0; i < strlen(format_string); i++) {
-    if ((format_string[i] == '#') && (format_string[i + 1] == 'k')) {
-      i++;
-
-      for (int j = 0; j < strlen(param); j++) {
-        char letter = param[j];
-        printChangedLetter(letter);
-      }
-    } else if ((format_string[i] == '#') && (format_string[i + 1] == '.')) {
-      i += 2;
-
-      char tab_with_number[8];
-      for (int j = 0; j < strlen(tab_with_number); j++) {
-        tab_with_number[j] = "";
-      }
-
-      int counter = 0;
-      for (int j = 0; j < strlen(tab_with_number); j++) {
-        if (format_string[i + j] == 'k') {
-          break;
-        }
-
-        if (format_string[i + j] >= '0' && format_string[i + j] <= '9') {
-          tab_with_number[j] = format_string[i + j];
-          counter++;
-        }
-      }
-      i += counter;
-
-
-      int number_from_string = atoi(tab_with_number);
-      int number_of_letter_to_display;
-
-      if (number_from_string > strlen(param)) {
-        number_of_letter_to_display = strlen(param);
-      } else {
-        number_of_letter_to_display = number_from_string;
-      }
-
-      for (int j = 0; j < number_of_letter_to_display; j++) {
-        char letter = param[j];
-        printChangedLetter(letter);
-      }
-
-    } else if(((format_string[i] == '#') && (format_string[i + 1] >= '0') && (format_string[i + 1] <= '9')) || ((format_string[i] == '#') && (format_string[i + 2] >= '0') && (format_string[i + 1] == '-') && (format_string[i + 2] <= '9'))) {
-
-      int flag = 0;
-      if(format_string[i + 1] == '-') {
+    if (format_string[i] == '#') {
+      if (format_string[i + 1] == 'k') {
         i++;
-        flag = 1;
-      }
-      i++;
+        for (int j = 0; j < strlen(param); j++) {
+          char letter = param[j];
+          printChangedLetter(letter);
+        }
+      } else if (format_string[i + 1] == '.') {
+        i += 2;
 
-      
+        char tab_with_number[8];
+        clearArray(tab_with_number);
 
-      char tab_with_number[8];
-      for (int j = 0; j < strlen(tab_with_number); j++) {
-        tab_with_number[j] = "";
-      }
+        int counter = 0;
+        for (int j = 0; j < strlen(tab_with_number); j++) {
+          if (format_string[i + j] == 'k') {
+            break;
+          }
 
-      int counter = 0;
-      for (int j = 0; j <= strlen(tab_with_number); j++) {
-        if (format_string[i + j] == 'k') {
-          break;
+          if (format_string[i + j] >= '0' && format_string[i + j] <= '9') {
+            tab_with_number[j] = format_string[i + j];
+            counter++;
+          }
+        }
+        i += counter;
+
+        int number_from_string = atoi(tab_with_number);
+        int number_of_letter_to_display;
+
+        if (number_from_string > strlen(param)) {
+          number_of_letter_to_display = strlen(param);
+        } else {
+          number_of_letter_to_display = number_from_string;
         }
 
-        if (format_string[i + j] >= 48 && format_string[i + j] <= 57) {
-          tab_with_number[j] = format_string[i + j];
-          counter++;
+        for (int j = 0; j < number_of_letter_to_display; j++) {
+          char letter = param[j];
+          printChangedLetter(letter);
         }
-      }
-      i += counter;
+      } else if ((format_string[i + 1] >= '0' && format_string[i + 1] <= '9') ||
+                 (format_string[i + 2] >= '0' && format_string[i + 1] == '-' &&
+                  format_string[i + 2] <= '9')) {
+        int flag = 0;
+        if (format_string[i + 1] == '-') {
+          i++;
+          flag = 1;
+        }
+        i++;
 
-      int number_of_space = atoi(tab_with_number);
-      if(number_of_space <= strlen(param) || flag == 1) {
-        number_of_space = 0;
-      } else {
-        number_of_space = number_of_space - strlen(param);
-      }
+        char tab_with_number[8];
+        clearArray(tab_with_number);
 
-      for(int i = 0; i< number_of_space; i++) {
-        putchar(32);
-      }
+        int counter = 0;
+        for (int j = 0; j <= strlen(tab_with_number); j++) {
+          if (format_string[i + j] == 'k') {
+            break;
+          }
 
-      for (int j = 0; j < strlen(param); j++) {
-        char letter = param[j];
-        printChangedLetter(letter);
+          if (format_string[i + j] >= 48 && format_string[i + j] <= 57) {
+            tab_with_number[j] = format_string[i + j];
+            counter++;
+          }
+        }
+        i += counter;
+
+        int number_of_space = atoi(tab_with_number);
+        if (number_of_space <= strlen(param) || flag == 1) {
+          number_of_space = 0;
+        } else {
+          number_of_space = number_of_space - strlen(param);
+        }
+
+        for (int i = 0; i < number_of_space; i++) {
+          putchar(32);
+        }
+
+        for (int j = 0; j < strlen(param); j++) {
+          char letter = param[j];
+          printChangedLetter(letter);
+        }
       }
     } else {
       putchar(format_string[i]);
@@ -107,7 +106,6 @@ int my_printf(char *format_string, char *param) {
   }
   puts("");
 }
-
 
 int main(int argc, char *argv[]) {
   char buf[1024], buf2[1024];
